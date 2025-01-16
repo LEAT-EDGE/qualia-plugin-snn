d158181e316f91575e46f608ac83ac5818f65ba0
piernov <piernov@piernov.org>
2025-01-16 19:10:09 +0100

    learningframework.SpikingJellyMultiStepTimeStepsInData: inherit from SpikingJellyTimeStepsInData and fix dimension reordering
    (cherry picked from commit 28e0840f9049abec6c0b59a59cbb3e7d691d967a)

--- src/qualia_plugin_snn/learningframework/SpikingJellyMultiStepTimeStepsInData.py
+++ src/qualia_plugin_snn/learningframework/SpikingJellyMultiStepTimeStepsInData.py
@@ -1,4 +1,6 @@
-"""Provide the SpikingJelly single-step with timesteps in input data learningframework module."""
+"""Provide the SpikingJelly multi-step with timesteps in input data learningframework module."""
+
+from __future__ import annotations
 
 import logging
 import sys
@@ -6,7 +8,7 @@ import sys
 import torch
 from spikingjelly.activation_based import functional  # type: ignore[import-untyped]
 
-from .SpikingJelly import SpikingJelly
+from .SpikingJellyTimeStepsInData import SpikingJellyTimeStepsInData
 
 if sys.version_info >= (3, 12):
     from typing import override
@@ -15,15 +17,15 @@ else:
 
 logger = logging.getLogger(__name__)
 
-class SpikingJellyMultiStepTimeStepsInData(SpikingJelly):
-    """SpikingJelly single-step with timesteps in data LearningFramework implementation extending SpikingJelly single-step."""
+class SpikingJellyMultiStepTimeStepsInData(SpikingJellyTimeStepsInData):
+    """SpikingJelly multi-step with timesteps in data LearningFramework implementation extending SpikingJelly single-step."""
 
-    class TrainerModule(SpikingJelly.TrainerModule):
-        """SpikingJelly single-step with timesteps in data TrainerModule extending SpikingJelly single-step TrainerModule."""
+    class TrainerModule(SpikingJellyTimeStepsInData.TrainerModule):
+        """SpikingJelly multi-step with timesteps in data TrainerModule extending SpikingJelly single-step TrainerModule."""
 
         @override
         def forward(self, x: torch.Tensor) -> torch.Tensor:
-            """Forward pass for a Spiking Neural Network model with timesteps in input data in single-step mode.
+            """Forward pass for a Spiking Neural Network model with timesteps in input data in multi-step mode.
 
             First calls SpikingJelly's reset on the model to reset neurons potentials.
             Call :meth:`qualia_plugin_snn.learningmodel.pytorch.SNN.SNN.forward` for each timestep of the input data.
@@ -37,13 +39,7 @@ class SpikingJellyMultiStepTimeStepsInData(SpikingJelly):
             functional.reset_net(self.model)
 
             # Switch timestep dim from 3rd to 1st place
-            if len(x.shape) == 5: # noqa: PLR2004 2D data, [N, C, T, H, W] → [T, N, C, H, W]
-                x = x.permute(2, 0, 1, 3, 4)
-            elif len(x.shape) == 4: # noqa: PLR2004 1D data [N, C, T, S] → [T, N, C, S]
-                x = x.permute(2, 0, 1, 3)
-            else:
-                logger.error('Unsupported number of axes in dataset: %s, must be 4 or 5', len(x.shape))
-                raise ValueError
+            x = x.swapaxes(0, 1)
 
             if x.shape[0] != self.model.timesteps:
                 logger.error('Model.timesteps differs from timesteps dimension in data: %s != %s',
