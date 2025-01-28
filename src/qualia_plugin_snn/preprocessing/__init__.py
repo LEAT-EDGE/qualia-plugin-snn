5d614ad750d8854665fc07f8904ef88f9c690db6
piernov <piernov@piernov.org>
2025-01-28 18:57:00 +0100

    Add preprocessing.IntegrateEventsByFixedDuration

--- src/qualia_plugin_snn/preprocessing/__init__.py
+++ src/qualia_plugin_snn/preprocessing/__init__.py
@@ -1,6 +1,7 @@
 """Qualia-Plugin-SNN preprocessing package contains preprocessing modules adapted for or dedicated to Spiking Neural Networks."""
 
 from .Group2TimeStepsBySample import Group2TimeStepsBySample
+from .IntegrateEventsByFixedDuration import IntegrateEventsByFixedDuration
 from .Split2TimeSteps import Split2TimeSteps
 
-__all__ = ['Group2TimeStepsBySample', Split2TimeSteps']
+__all__ = ['Group2TimeStepsBySample', 'IntegrateEventsByFixedDuration', 'Split2TimeSteps']
