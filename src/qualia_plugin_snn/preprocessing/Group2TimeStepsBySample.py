b72052531b754ed973fda3e15a8c266f0ee36804
piernov <piernov@piernov.org>
2025-10-29 16:01:17 +0100

    preprocessing.Group2TimeStepsBySample: handle chunked preprocessing

--- src/qualia_plugin_snn/preprocessing/Group2TimeStepsBySample.py
+++ src/qualia_plugin_snn/preprocessing/Group2TimeStepsBySample.py
@@ -8,8 +8,8 @@ import time
 from typing import Any
 
 import numpy as np
-from qualia_core.datamodel import RawDataModel
-from qualia_core.preprocessing.Preprocessing import Preprocessing
+from qualia_core.datamodel.RawDataModel import RawData, RawDataChunks, RawDataModel
+from qualia_core.preprocessing.Preprocessing import Preprocessing, iterate_generator
 
 if sys.version_info >= (3, 12):
     from typing import override
@@ -17,6 +17,7 @@ else:
     from typing_extensions import override
 
 logger = logging.getLogger(__name__)
+
 
 class Group2TimeStepsBySample(Preprocessing[RawDataModel, RawDataModel]):
     """Preprocessing module to group frame data from same sample into timesteps."""
@@ -29,6 +30,58 @@ class Group2TimeStepsBySample(Preprocessing[RawDataModel, RawDataModel]):
         super().__init__()
 
         self.__timesteps = timesteps
+
+    @iterate_generator
+    def __process_set(self, s: RawData, sname: str) -> RawData:
+        if s.info is None:
+            logger.error('Info for %s must be defined', sname)
+            raise ValueError
+
+        data_list: list[np.ndarray[Any, np.dtype[np.float32]]] = []
+        labels_list: list[np.ndarray[Any, Any]] = []
+        info_list: list[tuple[int, int]] = []
+
+        first = 0
+        last = 0
+        for sample in s.info:  # For each input sample
+            data = s.x[sample.begin:sample.end]
+            labels = s.y[sample.begin:sample.end]
+
+            frame_chunks: int = data.shape[0] // self.__timesteps
+            data = data[:frame_chunks * self.__timesteps]  # Truncate excessive frames
+            data = data.reshape((frame_chunks, self.__timesteps, *data.shape[1:]))  # N, T, H, W, C
+
+            labels = labels[:frame_chunks * self.__timesteps]  # Truncate excessive labels
+            labels = labels.reshape((frame_chunks, self.__timesteps, *labels.shape[1:]))  # Group labels, [N, T, …]
+
+            # One label per group of timesteps
+            new_labels: np.ndarray[Any, Any] = np.ndarray((frame_chunks, *labels.shape[2:]), dtype=labels.dtype)
+            # Find most common label in each group of timesteps to reduce over timesteps
+            for i, frame_labels in enumerate(labels):
+                values, counts = np.unique(frame_labels, return_counts=True)
+                new_labels[i] = values[counts.argmax()]
+
+            data_list.append(data)
+            labels_list.append(new_labels)
+
+            last += len(data)
+            info_list.append((first, last))
+            first = last
+
+        data_array = np.concatenate(data_list)
+        labels_array = np.concatenate(labels_list)
+        info_array = np.rec.array(info_list, dtype=np.dtype([('begin', np.int64), ('end', np.int64)]))
+
+        logger.info('Shapes: %s_x=%s, %s_y=%s, %s_info=%s',
+                    sname, data_array.shape,
+                    sname, labels_array.shape,
+                    sname, info_array.shape)
+
+        s.x = data_array
+        s.y = labels_array
+        s.info = info_array
+
+        return s
 
     @override
     def __call__(self, datamodel: RawDataModel) -> RawDataModel:
@@ -45,54 +98,13 @@ class Group2TimeStepsBySample(Preprocessing[RawDataModel, RawDataModel]):
         """
         start = time.time()
         for name, s in datamodel:
+            # Update the shape of RawDataChunks instance to get correct pre-allocation of output file
+            if isinstance(s, RawDataChunks) and s.shapes.data is not None:
+                # Add the timesteps dimension
+                # Total number of samples after transformation cannot be known at this time so leave undefined
+                s.shapes.data = (None, self.__timesteps, *s.shapes.data[1:])
 
-            if s.info is None:
-                logger.error('Info for %s must be defined', name)
-                raise ValueError
-
-            data_list: list[np.ndarray[Any, np.dtype[np.float32]]] = []
-            labels_list: list[np.ndarray[Any, Any]] = []
-            info_list: list[tuple[int, int]] = []
-
-            first = 0
-            last = 0
-            for sample in s.info: # For each input sample
-                data = s.x[sample.begin:sample.end]
-                labels = s.y[sample.begin:sample.end]
-
-                frame_chunks: int = data.shape[0] // self.__timesteps
-                data = data[:frame_chunks * self.__timesteps] # Truncate excessive frames
-                data = data.reshape((frame_chunks, self.__timesteps, *data.shape[1:])) # N, T, H, W, C
-
-                labels = labels[:frame_chunks * self.__timesteps] # Truncate excessive labels
-                labels = labels.reshape((frame_chunks, self.__timesteps, *labels.shape[1:])) # Group labels, [N, T, …]
-
-                # One label per group of timesteps
-                new_labels: np.ndarray[Any, Any] = np.ndarray((frame_chunks, *labels.shape[2:]), dtype=labels.dtype)
-                # Find most common label in each group of timesteps to reduce over timesteps
-                for i, frame_labels in enumerate(labels):
-                    values, counts = np.unique(frame_labels, return_counts=True)
-                    new_labels[i] = values[counts.argmax()]
-
-                data_list.append(data)
-                labels_list.append(new_labels)
-
-                last += len(data)
-                info_list.append((first, last))
-                first = last
-
-            data_array = np.concatenate(data_list)
-            labels_array = np.concatenate(labels_list)
-            info_array = np.rec.array(info_list, dtype=np.dtype([('begin', np.int64), ('end', np.int64)]))
-
-            logger.info('Shapes: %s_x=%s, %s_y=%s, %s_info=%s',
-                        name, data_array.shape,
-                        name, labels_array.shape,
-                        name, info_array.shape)
-
-            s.x = data_array
-            s.y = labels_array
-            s.info = info_array
+            setattr(datamodel.sets, name, self.__process_set(s, name))
 
         logger.info('Grouping to timesteps finished in %s s.', time.time() - start)
         return datamodel
